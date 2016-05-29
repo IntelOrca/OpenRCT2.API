fc26c9b9bffa3a678148ded095276fdef7fce624
Ted John <ted@brambles.org>
2016-05-29 21:26:25 +0100

    add GitHub push event hook

---
+++ src/OpenRCT2.API/Controllers/GithubController.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OpenRCT2.API.Controllers
+{
+    public class GithubController : Controller
+    {
+        public class JGitHubPushRequest
+        {
+            public string action { get; set; }
+        }
+
+        [Route("github/push")]
+        [HttpPost]
+        public void OnPush(JGitHubPushRequest request)
+        {
+            Process.Start("bash", "/home/openrct2/update.sh");
+        }
+    }
+}
