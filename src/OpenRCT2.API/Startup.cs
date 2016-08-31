952aa52b8bc1409b88cae6d593814dad7ed0ba2e
Miso Zmiric (Mike Squinter) <mzmiric5@users.noreply.github.com>
2016-08-31 03:39:14 +0200

    Allow all methods and headers to CORS for now
    We should probably edit this at some point later, to only allow specific methods and headers, once we have all the UI api requests defined

--- src/OpenRCT2.API/Startup.cs
+++ src/OpenRCT2.API/Startup.cs
@@ -68,7 +68,9 @@ namespace OpenRCT2.API
 #endif
 
             // Allow certain domains for AJAX / JSON capability
-            app.UseCors(builder => builder.WithOrigins(AllowedOrigins));
+            app.UseCors(builder => builder.WithOrigins(AllowedOrigins))
+                                          .AllowAnyHeader()
+                                          .AllowAnyMethod();
 
 #if _ENABLE_CHAT_
             app.Map("/chat", wsapp => {
