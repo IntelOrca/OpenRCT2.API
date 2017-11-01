4b8fe01d2bd6d59d4af549648555c8d8cde51b75
Ted John <ted@brambles.org>
2017-11-01 17:48:31 +0000

    Add object controller stub
    Committing this to master to check that deploying still works.

---
+++ src/OpenRCT2.API/Controllers/ObjectController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenRCT2.API.Abstractions;
+
+namespace OpenRCT2.API.Controllers
+{
+    public class ObjectController
+    {
+        [HttpGet("objects")]
+        public object GetAll()
+        {
+            return new
+            {
+                status = JStatus.OK,
+                objects = new[] { "GWTRITC", "GWTRJT1", "GWTRJT2" }
+            };
+        }
+    }
+}
