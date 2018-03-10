c1d87f9ff343814b510278f3939bb304235d6bf1
Ted John <ted@brambles.org>
2018-03-10 14:35:00 +0000

    Move profile endpoints to new controller

--- src/OpenRCT2.API/Controllers/UserController.cs
+++ src/OpenRCT2.API/Controllers/UserController.cs
@@ -2,16 +2,13 @@
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using OpenRCT2.API.Abstractions;
 using OpenRCT2.API.ActionFilters;
-using OpenRCT2.API.Authentication;
 using OpenRCT2.API.Extensions;
 using OpenRCT2.API.Implementations;
 using OpenRCT2.API.Models.Requests;
-using OpenRCT2.API.Services;
 using OpenRCT2.DB.Abstractions;
 using OpenRCT2.DB.Models;
 
@@ -139,70 +136,6 @@ namespace OpenRCT2.API.Controllers
             return JResponse.OK();
         }
 
-        [HttpPost("user/create")]
-        public async Task<object> CreateAsync(
-            [FromServices] GoogleRecaptchaService recaptchaService,
-            [FromServices] UserAccountService userAccountService,
-            [FromBody] CreateUserRequest body)
-        {
-            var remoteIp = HttpContext.Connection.RemoteIpAddress.ToString();
-            if (!await recaptchaService.ValidateAsync(remoteIp, body.Captcha).ConfigureAwait(false))
-            {
-                return BadRequest(JResponse.Error("reCAPTCHA validation failed."));
-            }
-            if (!await userAccountService.IsNameAvailabilityAsync(body.Username))
-            {
-                return BadRequest(JResponse.Error("User name already taken."));
-            }
-            if (!await userAccountService.IsEmailAvailabilityAsync(body.Email))
-            {
-                return BadRequest(JResponse.Error("Email address already registered."));
-            }
-            await userAccountService.CreateAccountAsync(body.Username, body.Email, body.Password);
-            return JResponse.OK();
-        }
-
-        [HttpPost("user/auth")]
-        public async Task<object> SignInAsync(
-            [FromServices] UserAuthenticationService userAuthenticationService,
-            [FromBody] SignInRequest body)
-        {
-            var (user, authToken) = await userAuthenticationService.AuthenticateAsync(body.Username, body.Password);
-            if (authToken == null)
-            {
-                var result = JResponse.Error("Invalid username or password.");
-                return StatusCode(StatusCodes.Status401Unauthorized, result);
-            }
-
-            return new {
-                status = JStatus.OK,
-                token = authToken.Token,
-                user = GetProfileInfo(user)
-            };
-        }
-
-        [Authorize]
-        [HttpGet("user/profile")]
-        public object GetProfileAsync()
-        {
-            var currentUser = User.Identity as AuthenticatedUser;
-            return new
-            {
-                status = JStatus.OK,
-                result = GetProfileInfo(currentUser.User)
-            };
-        }
-
-        [Authorize]
-        [HttpDelete("user/auth")]
-        public async Task<object> SignOutAsync(
-            [FromServices] UserAuthenticationService userAuthenticationService)
-        {
-            var currentUser = User.Identity as AuthenticatedUser;
-            await userAuthenticationService.RevokeTokenAsync(currentUser.Token);
-            return JResponse.OK();
-        }
-
         [HttpGet("users")]
         [Authorize(Roles = "Administrator")]
         public async Task<object> GetAllAsync(
@@ -226,18 +159,6 @@ namespace OpenRCT2.API.Controllers
                 emailMd5 = md5.ComputeHash(emailBytes).ToHexString();
             }
             return $"https://www.gravatar.com/avatar/{emailMd5}";
-        }
-
-        private static object GetProfileInfo(User user)
-        {
-            return new
-            {
-                name = user.Name,
-                permissions = new[]
-                {
-                    "news.write"
-                }
-            };
         }
     }
 }
