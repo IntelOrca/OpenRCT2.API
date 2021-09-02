9f039e942b5a5340d733b0cbe54eefb11bb4604a
Ted John <ted@brambles.org>
2021-09-02 22:42:05 +0100

    Add initial content web app

---
+++ src/OpenRCT2.Content/Pages/Author.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace OpenRCT2.Content.Pages
+{
+    public partial class Author
+    {
+        [Parameter]
+        public string Owner { get; set; }
+    }
+}
---
+++ src/OpenRCT2.Content/Pages/Upload.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace OpenRCT2.Content.Pages
+{
+    public partial class Upload
+    {
+        [Inject]
+        private NavigationManager Navigation { get; set; }
+
+        private string nameInput;
+        private string descriptionInput;
+
+        private void OnSubmit()
+        {
+            Navigation.NavigateTo("/");
+        }
+    }
+}
